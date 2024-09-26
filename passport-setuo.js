af3d65add59610fb6f694d0c0406afa11fa55b97
Fahmida Sherin <fahmidasherinck@gmail.com>
2024-09-26 11:43:53 +0530

    clientid,seceretId

--- passport-setuo.js
+++ passport-setuo.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENTID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://glamwear.site/auth/google/callback"
+    callbackURL: "https://glamwear.site/auth/google/callback",
 },
 
 async (accessToken, refreshToken, profile, done) => {
